a8829c15f70105d4f57c56ca0788661828946b2c
Toniloba Ogundare <tonilobaogundare@gmail.com>
2023-08-15 15:43:54 -0700

    main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - function to  print file name
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+#include "main.h"
+
+/**
+ * main - function to  print file name
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
